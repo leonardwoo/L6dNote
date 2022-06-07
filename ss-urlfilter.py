b8cfddfd1cd3e52390df577b7e64ae79ecad3eef
Leonard Woo <leo_916@yahoo.com>
2022-06-07 21:28:23 +0800

    Update ss-urlfilter.py

--- ss-urlfilter.py
+++ ss-urlfilter.py
@@ -6,7 +6,9 @@ import requests
 import base64
 from urllib.parse import urlparse
 from urllib.parse import unquote
+from urllib.parse import quote
 import re
+import os
 
 def req(url=''):
     headers = {
@@ -25,33 +27,41 @@ def find(regex='', input=''):
     return re.search(regex, input)
 
 
-def main():
-    SUBSCRIBE = ""
-    data = req(SUBSCRIBE)
-    data = base64.b64decode(data).decode('utf8')
+def subscribe_filter(data=''):
+    data = base64.b64decode(data.encode('utf8')).decode('utf8')
     sb = []
-
     for line in data.splitlines():
         urlobj = urlparse(line)
         lineName = unquote(urlobj.fragment)
-        lastBracket = lineName.rfind("[")
-        if lastBracket <= 0 :
-            lastBracket = len(lineName)
-        lineName = lineName[0:lastBracket]
+       # lastBracket = lineName.rfind("[")
+       # if lastBracket <= 0 :
+       #     lastBracket = len(lineName)
+       # lineName = lineName[0:lastBracket]
         authInfo = urlobj.username
-        authInfo = base64.b64decode(authInfo).decode('utf8')
-        host = urlobj.hostname
-        if find("^$", lineName) or find("", lineName) or host.lower() == "":
+        # authInfo = base64.b64decode(authInfo).decode('utf8')
+        host = urlobj.hostname + ":" + str(urlobj.port)
+        if find("^", lineName) or find("", host):
             continue
         
         lineName = "✈️" + lineName
+        # lineName = quote(lineName)
         ss = "ss://{0}@{1}#{2}".format(authInfo, host, lineName)
-        sb.append(base64.b64encode(ss.encode('utf8')).decode('utf8'))
+        sb.append(ss)
         sb.append('\n')
     
     sub = ''.join(sb)
-    data = base64.b64encode(sub.encode('utf8')).decode('utf8')
-    with open("ss-m.txt", "w+") as file:
+    return base64.b64encode(sub.encode('utf8')).decode('utf8')
+
+
+def main():
+    SUBSCRIBE = ""
+    FILE_PATHNAME = "ss-m.txt"
+    data = req(SUBSCRIBE)
+    data = subscribe_filter(data)
+    if os.path.exists(FILE_PATHNAME):
+        os.remove(FILE_PATHNAME)
+    
+    with open(FILE_PATHNAME, "w+") as file:
         file.write(data)
 
 
