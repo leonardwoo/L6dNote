5995eaf882890dc37d7e5d1258883f4aa63e02d8
Leonard Woo <leo_916@yahoo.com>
2022-10-09 13:58:13 +0800

    Create WebConfig.java

---
+++ spring/WebConfig.java
@@ -0,0 +1,30 @@
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Spring boot web config
+ */
+@Component
+public class WebConfig {
+  
+  /**
+   * CORS config
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+      }
+    };
+  }
+
+}
