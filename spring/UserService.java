7a8e90a0bfa7a24bc6b888be9938adaf8904a8b8
Leonard Woo <leo_916@yahoo.com>
2022-10-23 11:45:30 +0800

    Update UserService.java

--- spring/UserService.java
+++ spring/UserService.java
@@ -1,4 +1,5 @@
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements UserDetailsService {
 
-  @Autowire
+  @Autowired
   private UserMapper mapper;
   
   @Override
